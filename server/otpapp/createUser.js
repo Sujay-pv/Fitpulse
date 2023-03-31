ea141367f3f8934118c56e4672c792092b4afa51
Sujay PV <sujaylords100@gmail.com>
2023-03-31 20:19:06 +0530

    added email validation

--- server/otpapp/createUser.js
+++ server/otpapp/createUser.js
@@ -1,17 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const schema = mongoose.Schema({
-    name : {
-            type: String,
-            require: true
-    },
-    email : 'String',
-    mobileNumber : {
-            type: Number,
-            require: true
-        },
-    password : 'String',
-    // verificationotp : 'String',
-    // isOTPVerfied : Boolean
-})
-const usermodel = mongoose.model('usermodel',schema);
+  name: {
+    type: String,
+    require: true,
+  },
+  email: "String",
+  mobileNumber: {
+    type: Number,
+    require: true,
+  },
+  password: "String",
+  // verificationotp : 'String',
+  // isOTPVerfied : Boolean
+});
+
+schema.statics.isThisEmailInUse = async function (email) {
+  try {
+    const user = await this.findOne({ email });
+    if (user) return false;
+    return true;
+  } catch (error) {
+    console.log("error inside emailinuse function", error.message);
+    return false;
+  }
+};
+const usermodel = mongoose.model("usermodel", schema);
 module.exports = usermodel;
